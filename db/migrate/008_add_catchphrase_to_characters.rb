92dbeadd0b91c3339a09163cdb68082f0c0a0650
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-09 01:46:22 +0000

    automatically backed up by learn

--- db/migrate/008_add_catchphrase_to_characters.rb
+++ db/migrate/008_add_catchphrase_to_characters.rb
@@ -1,3 +1,5 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
-  add_column :characters, :catchphrase, :string
+  def change
+    add_column :characters, :catchphrase, :string
+  end
 end
