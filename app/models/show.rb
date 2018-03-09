ce1c79df1e6d3c4aa7372a04085239603d5658d2
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-09 02:44:06 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,14 +2,4 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
-
-  def build_network(call_letters)
-    # binding.pry
-  end
-
-  # def network
-  # end
-  #
-  # def call_letters
-  # end
 end
